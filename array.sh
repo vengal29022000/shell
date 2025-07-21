dee6a5f421183d5a6eee78955df244068af80aa1
gowthamflm <vengalareddy.challa@gmail.com>
2025-07-21 08:13:03 +0530

    committing4

---
+++ array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+fruits=("apple","banana","mango")
+
+echo " the first value ${fruits[0]}"
+echo " the first value ${fruits[1]}"
+echo " the first value ${fruits[2]}"
+echo " the first value ${fruits[3]}"
+
