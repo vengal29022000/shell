a3bc2225f66adb582763c65410413c5d6256bffe
gowthamflm <vengalareddy.challa@gmail.com>
2025-07-21 08:14:28 +0530

    committing5

--- array.sh
+++ array.sh
@@ -3,7 +3,7 @@
 fruits=("apple","banana","mango")
 
 echo " the first value ${fruits[0]}"
-echo " the first value ${fruits[1]}"
-echo " the first value ${fruits[2]}"
-echo " the first value ${fruits[3]}"
+echo " the second value ${fruits[1]}"
+echo " the third value ${fruits[2]}"
+echo " the fourth value ${fruits[3]}"
 
